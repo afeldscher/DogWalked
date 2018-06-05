1e77295fa39fac724220fd65cf922c475a59ca9c
Adam Feldscher <aff39@drexel.edu>
2018-06-04 23:13:20 -0400

    add swipes

--- DogWalked/Scripts/app/scripts.js
+++ DogWalked/Scripts/app/scripts.js
@@ -73,10 +73,11 @@ function createDog() {
 }
 
 
+
+//-----Create Dog Image------
 function uploadImg() {
     $("#createDog #imageUpload").click();
 }
-
 
 function readURL(input) {
     if (input.files && input.files[0]) {
@@ -90,4 +91,48 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+
+
+
+//-----Swipe-----
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+var xDown = null;
+var yDown = null;
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+};
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    if (Math.abs(xDiff) + Math.abs(yDiff) > 100) { //to deal with to short swipes
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+            if (xDiff > 0) {/* left swipe */
+                rightarrow();
+            } else {/* right swipe */
+                leftarrow();
+            }
+        } else {
+            if (yDiff > 0) {/* up swipe */
+                alert('Up!');
+            } else { /* down swipe */
+                alert('Down!');
+            }
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
+    }
+};
+
 
