209d570780280f4e2264ccfdc6c742f70ffc26b5
Adam Feldscher <aff39@drexel.edu>
2018-06-04 23:02:21 -0400

    Working

--- DogWalked/Scripts/app/scripts.js
+++ DogWalked/Scripts/app/scripts.js
@@ -1,7 +1,13 @@
 ﻿
 page = 0;
+newUrl = "/DogWalked/Content/Logo.png";
 
 function leftarrow() {
+    //if there is no previous page exit
+    if ($("#dog" + (page - 1)).length == 0) {
+        return;
+    }
+
     $("#dog" + page).hide();
     page--;
     $("#dog" + page).show()
@@ -9,6 +15,11 @@ function leftarrow() {
 
 
 function rightarrow() {
+    //if there is no next page exit
+    if ($("#dog" + (page + 1)).length == 0) {
+        return;
+    }
+
     $("#dog" + page).hide();
     page++;
     $("#dog" + page).show();
@@ -19,3 +30,64 @@ function iconClick(item) {
         .animate({ backgroundColor: "#FFFFFF" }, 500);
     $(item + 'Dot').fadeToggle();
 }
+
+
+//----Setup Arrow Key navigation----
+document.onkeyup = function (e) {
+    e = e || window.event;
+    if (e.keyCode == '37') {
+        leftarrow();
+    }
+    else if (e.keyCode == '39') {
+        rightarrow();
+    }
+}
+
+
+function createDog() {
+    if ($("#createDog #dogName").val().trim() == "") {
+        $("#createDog #dogName").css("background-color", "#e4868f");
+        return;
+    }
+
+    //Rename create page to +1
+    $("#dog" + page).attr("id", "dog" + (page + 1));
+    page++;
+
+    //---Create new Dog---
+    $newDog = $("#dog" + (page - 2)).clone(true);
+    $newDog.attr("id", "dog" + (page - 1));
+    $newDog.find(".dogName").html($("#createDog #dogName").val());
+    $newDog.find(".dogImg").attr("src", newUrl);
+
+    //---Insert New dog and go to it---
+    $("#dogs .container").append($newDog);
+    leftarrow();
+
+
+    //---Reset text boxes---
+    $("#createDog #dogName").css("background-color", "#fff");
+    $("#createDog #dogName").val("");
+    newUrl = "/DogWalked/Content/Logo.png";
+    $("#createDog #createDogLogo").attr('src', newUrl);
+}
+
+
+function uploadImg() {
+    $("#createDog #imageUpload").click();
+}
+
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("#createDog #createDogLogo").attr('src', e.target.result);
+            newUrl = e.target.result;
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
